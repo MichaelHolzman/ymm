285d0d843efc2db5216880b0d3a33f85ec78892d
MichaelHolzman <MichaelHolzman@de30bd6a-67a4-11de-873c-fb8c264d332a>
2009-08-26 14:12:48 +0000

    Poison check is added

--- yamm_tune.c
+++ yamm_tune.c
@@ -1,17 +1,19 @@
-#include <time.h>  /* For leak check based on time */
+#include <time.h>   /* For leak check based on time */
+#include <limits.h> /* For LONG_MAX */
 
-extern int yamm_max_thread_num  = 100;
-extern int yamm_make_statistics = 0;
-extern int yamm_debug_level     = 0;
-extern int yamm_double_free     = 0;
-extern int yamm_sizes[] = { 21, 33, 65, 73, 89, 98, 165, 169, 209, 257, 304, 577, 2264, 4513, 20000, 30000, 100000, 200000, 300000, 400000, 500000, -1};
-extern int yamm_is_dumper_on = 0;
-extern int yamm_dump_interval = 900;
+int yamm_max_thread_num  = 100;
+int yamm_make_statistics = 0;
+int yamm_debug_level     = 0;
+int yamm_double_free     = 0;
+int yamm_sizes[] = { 21, 33, 65, 73, 89, 98, 165, 169, 209, 257, 304, 577, 2264, 4513, 20000, 30000, 100000, 200000, 300000, 400000, 500000, -1};
+int yamm_is_dumper_on = 0;
+int yamm_dump_interval = 900;
+int yamm_poison_check_enabled = 1;
 
 #ifdef LINUX
-long long yamm_leak_check_start_time = 1;
-long long yamm_leak_check_stop_time = 10;
+unsigned long long yamm_leak_check_start_time = LONG_MAX;
+unsigned long long yamm_leak_check_stop_time = 10;
 #else
-extern hrtime_t yamm_leak_check_start_time = 1;
-extern hrtime_t yamm_leak_check_stop_time = 2;
+hrtime_t yamm_leak_check_start_time = 1;
+hrtime_t yamm_leak_check_stop_time = 2;
 #endif
