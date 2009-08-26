25f7d92e5eac8f6ebe0bf3d04e2172d1bda86c3e
MichaelHolzman <MichaelHolzman@de30bd6a-67a4-11de-873c-fb8c264d332a>
2009-08-26 14:10:45 +0000

    Poison check is added

--- trunk/yamm_leak_report.c
+++ trunk/yamm_leak_report.c
@@ -1,8 +1,19 @@
+#include <libgen.h>
+
 #include "yamm_leak_hp.c"
 #include "yamm_leak_linux.c"
 
 int main(int argc, char* argv[])
 {
-    ReadAndProcessData();
+    if(argc < 2)
+    {
+        printf("Usage: %s <data file name>\n", basename(argv[0]));
+        exit(EXIT_SUCCESS);
+    }
+
+    if(strcmp("yamm_leak_report", basename(argv[0])) == 0)
+        ReadAndProcessData(LEAK_MODE, argv[1]);
+    else
+        ReadAndProcessData(POISON_MODE, argv[1]);
     return 1;
 }
